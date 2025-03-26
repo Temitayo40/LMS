8e8ae03d9e76fbe40a1b9161895ccfd90158e31e
Damilola Adebowale <adebowaletemitayo40@gmail.com>
2025-03-26 11:30:07 +0100

    Add Stripe webhook handling for payment processing and status updates

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhooks } from "./controllers/webhook.js";
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhook.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import { clerkMiddleware } from "@clerk/express";
@@ -21,6 +21,7 @@ app.post("/clerk", express.json(), clerkWebhooks);
 app.use("/api/educator", express.json(), educatorRouter);
 app.use("/api/course", express.json(), courseRouter);
 app.use("/api/user", express.json(), userRouter);
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
